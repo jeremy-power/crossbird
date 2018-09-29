4603c486ee35e7749d24454da169f7824b6da934
jeremy-power <reaper9998@hotmail.com>
2018-09-29 01:02:31 -0400

    updated database code

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # Work with Python 3.6
 import discord
 from bot_functions import *
+from db import *
 
 with open('token.txt') as f:
     read_data = f.read()
@@ -9,23 +10,29 @@ TOKEN = read_data
 
 client = discord.Client()
 
-def create_user(userid):
-    ADD_USER_TO_DATABASE
-    return
+def create_user(discord_id, discord_name):
+    connection = getConnection()
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO datUsers(DiscordID, DiscordName) VALUES (" + discord_id + ", '" + discord_name +"');")
+    connection.commit()
 
-def enter_score(userid, score, date):
-    user_found = 0
-    for i in DATABASE_OF_USERS:
-        if userid == 1:
-            user_found = 1
-    if user_found = 0:
-        ADD_USER_TO_DATABASE
+def enter_score(discord_id, discord_name, score, date, isArchive):
+    #find the user by their discord ID
+    user = select_user_by_id(discord_id)
+    #if user doesn't exist yet, create one
+    if len(user) == 0:
+        create_user(discord_id, discord_name)
+    #otherwise if their name has changed, update it
+    elif user[0]['DiscordName'] != discord_name:
+        update_name(discord_id, discord_name)
+    #make sure user exists now
+    user = select_user_by_id(discord_id)
+    if len(user) != 0:
+        #if it does, add the score
+        create_score(discord_id, score, date, isArchive)
+        return 1
     else:
-        if NO_ENTRY_FOR_GIVEN_DATE_YET:
-            ADD_ENTRY_WITH_ID_SCORE_DATE
-            return(1)
-        else:
-            return(0)
+        return 0
 
 @client.event
 async def on_message(message):
--- db.py
+++ db.py
@@ -1,9 +1,39 @@
 import pyodbc
-
+import datetime
 def getConnection():
     return pyodbc.connect('Driver={SQL Server};Server=den1.mssql6.gear.host;Database=crossnerd;UID=crossnerd;PWD=powerj@@;')
 
-# cursor = connection.cursor()
-# cursor.execute("INSERT INTO datUsers(DiscordID, DiscordName) VALUES (1, 'test');")
-# connection.commit()
+def build_dict(cursor):
+    columns = [column[0] for column in cursor.description]
+    results = []
+    for row in cursor.fetchall():
+        results.append(dict(zip(columns,row)))
+    return results
 
+def select_all_users():
+    connection = getConnection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM datUsers")
+    return build_dict(cursor)
+
+def select_user_by_id(discord_id):
+    connection = getConnection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT TOP 1 * FROM datUsers WHERE DiscordID=" + str(discord_id))
+    return build_dict(cursor)
+
+def update_name(discord_id, discord_name):
+    connection = getConnection()
+    cursor = connection.cursor()
+    cursor.execute("UPDATE datUsers SET DiscordName = '" + discord_name + "' WHERE DiscordID = " + str(discord_id))
+    cursor.commit()
+
+def create_score(discord_id, score, date, isArchive):
+    user = select_user_by_id(discord_id)
+    user_id = user[0]['UserID']
+    connection = getConnection()
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO datScores(Score, UserID, Day, DateRecorded, isArchive) VALUES (?, ?, ?, ?, ?)",
+                  (score, user_id, date, datetime.datetime.now(), int(isArchive)))
+    cursor.commit()
+create_score(1, 10, datetime.datetime.now(), True)
