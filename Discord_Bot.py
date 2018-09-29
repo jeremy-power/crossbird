79df85b5b776f2f1be3b62931ce83adc700bd265
logan groves <logan groves>
2018-09-28 22:05:57 -0400

    Added date-scraping and time-handling functions to bot

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -1,5 +1,7 @@
 # Work with Python 3.6
 import discord
+import urllib3
+http = urllib3.PoolManager()
 
 with open('token.txt') as f:
     read_data = f.read()
@@ -7,6 +9,32 @@ f.closed
 TOKEN = read_data
 
 client = discord.Client()
+
+def date_scrape():
+    r = http.request(
+        'GET',
+        'https://www.nytimes.com/crosswords/game/mini'
+    )
+    r = str(r.data)
+    r = r.split('<title data-react-helmet="true">',1)
+    r = r[1]
+    r = r.split(' Daily Mini Crossword Puzzle',1)
+    r = r[0]
+    return(r)
+
+def time_to_number(time):
+    time = time.split(':')
+    time_len = len(time)
+    if time_len == 1:
+        time = int(time[0])
+    elif time_len == 2:
+        time = int(time[0])*60+int(time[1])
+    elif time_len == 3:
+        time = int(time[0])*3600+int(time[1])*60+int(time[2])
+    else:
+        time = -1
+    return(time)
+
 
 @client.event
 async def on_message(message):
