40a7f303baafd48a04387895fe49002d235ec68d
logan groves <logan groves>
2018-09-28 21:08:12 -0400

    Included rudimentary bot code

---
+++ Discord_Bot.py
@@ -0,0 +1,28 @@
+# Work with Python 3.6
+import discord
+
+with open('token.txt') as f:
+    read_data = f.read()
+f.closed
+TOKEN = read_data
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!crossword'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
